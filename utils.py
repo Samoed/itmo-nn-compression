3249f419dacb55dfff970002b8b52394a6ce5a45
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2023-09-21 14:45:25 +0300

    add pruning

---
+++ utils.py
@@ -0,0 +1,13 @@
+from torch.nn import Module
+
+
+def get_size(model: Module) -> float:
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    return size_all_mb

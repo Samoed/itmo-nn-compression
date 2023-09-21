18ed31bc7aadf14c7bcfe92bc8ad0c19636e2891
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2023-09-21 14:53:28 +0300

    add more amounts

--- prune/pruning.py
+++ prune/pruning.py
@@ -9,25 +9,25 @@ import mlflow
 
 mlflow.set_tracking_uri("http://localhost:5000")
 
+for amount in [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4]:
+    with mlflow.start_run(run_name=f"yolo prune {amount} params") as run:
+        model = YOLO("yolov8n.pt")
 
-with mlflow.start_run(run_name="yolo prune .3 params") as run:
-    model = YOLO("yolov8n.pt")
+        zeros = 0
+        n_elements = 0
+        for name, module in model.named_modules():
+            if isinstance(module, torch.nn.Conv2d):
+                prune.l1_unstructured(module, name="weight", amount=amount)
+                prune.remove(module, "weight")
+                zeros += float(torch.sum(module.weight == 0))
+                n_elements += float(module.weight.nelement())
+        mlflow.log_metric("size", get_size(model))
+        mlflow.log_metric("sparsity", 100.0 * zeros / n_elements)
 
-    zeros = 0
-    n_elements = 0
-    for name, module in model.named_modules():
-        if isinstance(module, torch.nn.Conv2d):
-            prune.l1_unstructured(module, name="weight", amount=0.3)
-            prune.remove(module, "weight")
-            zeros += float(torch.sum(module.weight == 0))
-            n_elements += float(module.weight.nelement())
-    mlflow.log_metric("size", get_size(model))
-    mlflow.log_metric("sparsity", 100.0 * zeros / n_elements)
-
-    metrics = model.val(data="coco128.yaml")
-    for key, val in metrics.results_dict.items():
-        new_key = key.replace("(", "").replace(")", "")
-        mlflow.log_metric(new_key, val)
-    for key, val in metrics.speed.items():
-        new_key = key.replace("(", "").replace(")", "")
-        mlflow.log_metric(new_key, val)
+        metrics = model.val(data="coco128.yaml")
+        for key, val in metrics.results_dict.items():
+            new_key = key.replace("(", "").replace(")", "")
+            mlflow.log_metric(new_key, val)
+        for key, val in metrics.speed.items():
+            new_key = key.replace("(", "").replace(")", "")
+            mlflow.log_metric(new_key, val)

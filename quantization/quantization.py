18baeb2de6ed785037b7663ff642e5d097600d7a
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2023-09-23 00:05:25 +0300

    add quantization
    Co-Authored-By: daniilrusov <144834417+daniilrusov@users.noreply.github.com>

---
+++ quantization/quantization.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+import sys
+
+sys.path.append("../")
+from utils import get_size
+import mlflow
+
+mlflow.set_tracking_uri("http://localhost:5000")
+
+
+def load_and_val(model_path: str):
+    model = YOLO(model_path, task='detect')
+    result = model.val(data='coco128.yaml', imgsz=640)
+    return result
+
+
+model = YOLO("yolov8n.pt")
+
+params = [("fp32", {}), ("fp16", {"half": True}), ("int8", {"int8": True})]
+for model_name, param in params:
+    with mlflow.start_run(run_name=f"yolo tflight {model_name}") as run:
+        model_path = model.export(format='tflite', **param)
+        load_and_val(model_path)
+
+        mlflow.log_metric("size", get_size(model))
+
+        metrics = model.val(data="coco128.yaml")
+        for key, val in metrics.results_dict.items():
+            new_key = key.replace("(", "").replace(")", "")
+            mlflow.log_metric(new_key, val)
+        for key, val in metrics.speed.items():
+            new_key = key.replace("(", "").replace(")", "")
+            mlflow.log_metric(new_key, val)
